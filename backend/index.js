8fd70435b83e91a93e14fac1bc2ccd6c70722857
Joanna Coll <johannam999@gmail.com>
2018-06-17 00:14:04 -0700

    some changes

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
-import express from 'express';
-import superagent from 'superagent';
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
 
 const app = express();
 
 require('dotenv').config();
-const GOOGLE_OAUTH_URL = '';
-const OPEN_ID_URL = '';
+
+const GOOGLE_OAUTH_URL = 'https://www.googleapis.com/oauth2/v4/token';
+const OPEN_ID_URL = 'https://www.googleapis.com/plus/v1/people/me/openIdConnect';
 
 app.get('/oauth/google', (request, response) => {
+  console.log(request.query.code);
   if (!request.query.code) {
     response.redirect(process.env.CLIENT_URL);
   } else {
-    console.log('__CODE__ STEP 3.1', request.query.code);
-    
-    console.log('__CODE__ STEP 3.2 SENDING THE CODE BACK__')
+    console.log('SENDING THE CODE BACK__');
     return superagent.post(GOOGLE_OAUTH_URL)
       .type('form')
       .send({
@@ -21,12 +23,11 @@ app.get('/oauth/google', (request, response) => {
         grant_type: 'authorization_code',
         client_id: process.env.GOOGLE_OAUTH_ID,
         client_secret: process.env.GOOGLE_OAUTH_SECRET,
-        redirect_uri: `${process.env.API_URL}/oauth/google` // we specify where we want token to go so google would send it there
+        redirect_uri: `${process.env.API_URL}/oauth/google`, // we specify where we want token to go so google would send it there
       })
-      .then(tokenResponse => {
-     
-        console.log('__STEP 3.3 - ACCESS TOKEN__');
-        if(!tokenResponse.body.access_token) { // in case we didn't pass verification
+      .then((tokenResponse) => {
+        console.log('- ACCESS TOKEN__');
+        if (!tokenResponse.body.access_token) { // in case we didn't pass verification
           response.redirect(process.env.CLIENT_URL);
         }
         const accessToken = tokenResponse.body.access_token;
@@ -34,20 +35,20 @@ app.get('/oauth/google', (request, response) => {
         return superagent.get(OPEN_ID_URL)
           .set('Authorization', `Bearer ${accessToken}`);
       })
-      .then(openIDResponse => {
-        console.log('__STEP 4 - OPEN ID __')
-          console.log(openIDResponse.body);
+      .then((openIDResponse) => {
+        console.log(openIDResponse.body);
           
-        response.cookie('_TOKEN')
+        response.cookie('_TOKEN');
         response.redirect(process.env.CLIENT_URL);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-        response.redirect(process.env.CLIENT_URL + '?error=oauth'); // we should always redirect user to their page
+        response.redirect(`${process.env.CLIENT_URL  }?error=oauth`); // we should always redirect user to their page
       });
   }
+  return undefined;
 });
 
 app.listen(process.env.PORT, () => {
-  console.log('__SERVER IS UP__ PORT', process.env.PORT);
+  console.log('__SERVER IS UP__');
 });
